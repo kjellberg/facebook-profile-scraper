488e3671de51d6ea094ac501ccfd1e1aaced5777
Rasmus Kjellberg <rk@youngmedia.se>
2016-08-24 15:39:49 +0200

    Removed demo file

--- demo.rb
+++
@@ -1,4 +0,0 @@
-require_relative "lib/facebook-profile-scraper"
-
-profile = FacebookProfileScraper.new('https://www.facebook.com/zuck') # Download profile
-puts profile.picture if profile.picture # => "Palo Alto, California"
