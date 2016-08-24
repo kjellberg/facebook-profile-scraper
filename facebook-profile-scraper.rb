d6cb8557ee77a89031b6a5567b4e060ab9ba336c
Rasmus Kjellberg <rk@youngmedia.se>
2016-08-24 14:23:18 +0200

    some cleaning

--- facebook-profile-scraper.rb
+++ facebook-profile-scraper.rb
@@ -3,16 +3,14 @@ require 'Nokogiri'
 
 class FacebookProfileScraper
   def initialize(facebook_profile)
-    # Set URL to Facebook profile
-    @facebook_profile = facebook_profile
     # Download page (GET REQEUST)
     @page = HTTParty.get(facebook_profile)
 
     # Parse the page with Nokogiri
-    @parse_page = Nokogiri::HTML(@page)
+    @parses_page = Nokogiri::HTML(@page)
   end
   def city
     # Parse first city from profile
-    @parse_page.css('span._50f5._50f7')[0].css('a').text
+    @parsed_page.css('span._50f5._50f7')[0].css('a').text
   end
 end
